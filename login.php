6ebca76d5c24dca09480da87df5cef69c3e9d51d
Napawuth Homsa-ard <104339012@student.swin.edu.au>
2026-05-06 17:03:47 +1000

    Fixed html validation error on login page

--- login.php
+++ login.php
@@ -9,10 +9,10 @@
     <?php include 'header.inc'; ?>
     <form method="post" action="process.php">
         <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
+        <input  id="username" type="text" name="username" required><br>
 
         <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
+        <input id="password" type="password" name="password" required><br>
 
         <input type="hidden" name="token" value="abc123">
         <input type="submit" value="Login">
